803a31cc26c5a00ccbf440b58508aa72d6d2d94a
Matt Licholai <mattlic@outlook.com>
2019-02-10 15:54:16 -0800

    my clockface works as desired - busy and ugly, but it works

--- Projects/learning_mod/app/index.js
+++ Projects/learning_mod/app/index.js
@@ -10,6 +10,9 @@ import {
   calculateDistance,
   zeroPad,  
   pickImage,
+  hoursToAngle,
+  minutesToAngle,
+  secondsToAngle,
  } from "../common/utils";
 
 // Update the clock every minute
@@ -20,8 +23,8 @@ const backImage = document.getElementById("bkimage");
 console.log("backImage is: " + backImage.href);
 
 // for testing adding the image
-backImage.href = "images/hot-asian.jpg" ;
-console.log("backImage is: " + backImage.href);
+// backImage.href = "images/hot-asian.jpg" ;
+// console.log("backImage is: " + backImage.href);
 
 // Get a handle on the <text> elements
 const lblDate = document.getElementById("date");
@@ -31,6 +34,11 @@ const lblFloors = document.getElementById("floors");
 const lblSteps = document.getElementById("steps");
 const lblDist = document.getElementById("dist");
 const lblHr = document.getElementById("hr");
+
+// Get handles on the analog clock parts
+const hourHand = document.getElementById("hourHand");
+const minHand = document.getElementById("minHand");
+const secHand = document.getElementById("secHand");
 
 // Get a handle on the other elements
 const leftArc = document.getElementById("leftArc");
@@ -51,6 +59,7 @@ clock.ontick = (evt) => {
   const dayOfMonth = now.getDate();
   const hours = now.getHours();
   const mins = zeroPad(now.getMinutes());  
+  const secs = now.getSeconds();
   
   const {clockDisplay} = preferences;
   const displayHours = clockDisplay === "12h" ? hours % 12 || 12 : zeroPad(hours);
@@ -58,18 +67,18 @@ clock.ontick = (evt) => {
   const displayDay = getDisplayDay(day);
 
   const charge = battery.chargeLevel;
-  console.log(Math.floor(battery.chargeLevel) + "%");
+  // console.log(Math.floor(battery.chargeLevel) + "%");
 
   // update background image
   backImage.href = pickImage( hours );
   
 
   // set and display the arcs for charge based on percentage
-  if (charge > 60) {
+  if (charge > 65) {
     // set arc color to greenyellow
     leftArc.style.fill = "greenyellow";
     rightArc.style.fill = "greenyellow";
-  } else if (charge > 30) {
+  } else if (charge > 35) {
     // set arc color to ornage
     leftArc.style.fill = "#FF5A00";
     rightArc.style.fill = "#FF5A00";
@@ -79,21 +88,26 @@ clock.ontick = (evt) => {
     rightArc.style.fill = "red";
   }
   
+  // Update the battery power arcs
   leftArc.startAngle = -180 +((180 - (1.8 * charge))/2);
   leftArc.sweepAngle = (1.8 * charge);
 
   rightArc.startAngle = 0 + ((180 - (1.8 * charge)) /2);
   rightArc.sweepAngle = (1.8 * charge);
 
+  // Update analog clock face based on the time
+  hourHand.groupTransform.rotate.angle = hoursToAngle(hours, mins);
+  minHand.groupTransform.rotate.angle = minutesToAngle(mins);
+  secHand.groupTransform.rotate.angle = secondsToAngle(secs);
+
+  // Update the digital clock face
   lblDate.text = `${zeroPad(dayOfMonth)} ${diplayMonth}`;
   lblDay.text = `${displayDay}`;
   lblTime.text = `${displayHours}:${mins}`;
   
+  // Update the activity status text
   lblFloors.text = (today.local.elevationGain || 0 );
   lblSteps.text = (today.local.steps || 0);
-  
-  // lblDist.text = calculateDistance((2300), "miles");
   lblDist.text = calculateDistance((today.local.distance || 0), "miles");
-  // lblHr.text = "wwwwwwww";
   
 }
